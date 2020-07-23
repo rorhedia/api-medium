121c6f6cf84af99ba239e8880c2befb773b7d8c9
codigoVis <etneciv2@icloud.com>
2020-07-23 01:55:43 -0500

    ADD method delete and patch

--- src/usecases/entries.usecase.js
+++ src/usecases/entries.usecase.js
@@ -5,7 +5,16 @@ const getAll = () => Entries.find()
 
 const createPost = post => Entries.create(post)
 
+function findByIdAndDelete(deleteEntries){
+    return Entries.findByIdAndDelete(deleteEntries)
+}
+function findByIdAndUpdate(idEntries, upDateEntries){
+    return Entries.findByIdAndUpdate(idEntries, upDateEntries)
+}
+
 module.exports = {
     getAll,
-    createPost
+    createPost,
+    findByIdAndDelete,
+    findByIdAndUpdate
 }
